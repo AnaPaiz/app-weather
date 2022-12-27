3bc24d8b75feb6c7c893058ca05ad1d8626dc34c
Ana Paiz Rivera <54344320+AnaPaiz@users.noreply.github.com>
2022-12-27 16:48:43 -0600

    Add files via upload

---
+++ js/backgroundWather.js
@@ -0,0 +1,18 @@
+ export const img_backgrounds = {
+    "01d": ["clear-sky-day.jpg", "Cielo despejado"],
+    "01n": ["clear-sky-night.jpg", "Cielo despejado"],
+    "02d": ["few-clouds-day.jpg", "Pocas nubes"],
+    "02n": ["few-clouds-night", "Pocas nubes"],
+    "03d": ["scattered-clouds-day.jpg", "Nubes dispersas"],
+    "03n":["scattered-cloud-night.jpg", "Nubes dispersas"], 
+    "04d": ["broken-clouds-day.jpg", "Nubes dispersas" ], 
+    "04n": ["clouds-broken-night.jpg", "Nubes dispersas"],
+    "09d": ["rain-day.jpg", "Chubascos"], 
+    "09n": ["rain-night.jpg", "Chubascos" ],
+    "10d": ["rain-day.jpg", "Día con Lluvia"],
+    "10n":["rain-night.jpg", "Noche con Lluvia"],
+    "11d": ["thunderstorm.jpg", "Tormenta"],
+    "11n": ["thunderstorm.jpg", "Tormenta"],
+    "13d": ["snow-day.jpg", "Nevada"],
+    "13n": ["snow-night.jpg", "Nevada"]    
+};
